2223cc23fb3f9cf7302e651de0a97188d54a439d
jaans21 <jaans21@icloud.com>
2025-07-13 18:55:43 +0200

    Clean up documentation and development files
    Remove unnecessary markdown files and development notes to keep repository clean

--- run_game.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Tic-Tac-Toe Game...
-echo.
-echo Make sure LOVE2D is installed on your system.
-echo If you get an error, install LOVE2D from https://love2d.org/
-echo.
-pause
-love .
-pause
